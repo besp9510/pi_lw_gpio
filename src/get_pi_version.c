459719dc0f17a09228eb9e05e22b656f940eb3f4
Benjamin Spencer <spencerbl77@yahoo.com>
2021-11-27 22:04:38 -0800

    added licencsing + added CM3

--- src/get_pi_version.c
+++ src/get_pi_version.c
@@ -1,3 +1,26 @@
+// Lightweight GPIO Interface Library for the Raspberry Pi
+//
+// Copyright (c) 2020 Benjamin Spencer
+// ============================================================================
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+// ============================================================================
+
 // Include C standard libraries:
 #include <stdio.h>  // C Standard I/O libary
 #include <stdlib.h> // C Standard library
@@ -158,6 +181,18 @@ static struct pi_version_struct pi_version[] = {
     // PI 3 Model B+
     {
         .revision_string = "a020d3",
+        .version = 3
+    },
+
+    // Computer Module 3
+    {
+        .revision_string = "a020a0",
+        .version = 3
+    },
+
+    // Computer Module 3+
+    {
+        .revision_string = "a02100",
         .version = 3
     },
 
